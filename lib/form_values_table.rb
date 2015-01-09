7227c1a78caa4213bd4e17f989167131d8dd1ebc
Joe Roberts: the greatest man alive <jrobe106@jhmi.edu>
2015-01-09 05:54:26 -0500

    remove pry binding

--- lib/form_values_table.rb
+++ lib/form_values_table.rb
@@ -1,7 +1,6 @@
 class FormValuesTable < ActiveRecord::Migration
 
   def self.create_values_table form_id
-    binding.pry
     if form_id.class == Fixnum
       create_table ('form_' + form_id.to_s).to_sym, id: false do |t|
         t.primary_key :entry_id
