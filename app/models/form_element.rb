433db05aa65a4b1f2d21b12ec2cec73d8a13d9ae
Joe Roberts: the greatest man alive <jrobe106@jhmi.edu>
2015-01-16 11:20:18 -0500

    form_element validations

--- app/models/form_element.rb
+++ app/models/form_element.rb
@@ -1,3 +1,7 @@
 class FormElement < ActiveRecord::Base
   belongs_to :form
+
+  validates :element_id,        presence: true
+  validates :element_name,      presence: true
+  validates :element_type_id,   presence: true
 end
