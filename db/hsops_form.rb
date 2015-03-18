fe95816319898237d13cebc0c7f5bcf05ebfe2d9
Michael Cohen <mrgcohen@gmail.com>
2015-03-18 14:52:57 -0400

    -oops should be 25

--- db/hsops_form.rb
+++ db/hsops_form.rb
@@ -17,7 +17,7 @@ fe = FormElement.create form_id: form.id, element_type_id: ElementType.find_by_e
 
 
 # section information
-fe = FormElement.create form_id: form.id, element_type_id: ElementType.find_by_e_type("section").id, element_id: 2, element_name: "element_2_1", element_title: "SECTION A: Your Work Area/Unit", element_tool_tip: "In this survey, think of your “unit” as the work area, department, or clinical area of the hospital where you spend most of your work time or provide most of your clinical services."
+fe = FormElement.create form_id: form.id, element_type_id: ElementType.find_by_e_type("section").id, element_id: 25, element_name: "element_2_1", element_title: "SECTION A: Your Work Area/Unit", element_tool_tip: "In this survey, think of your “unit” as the work area, department, or clinical area of the hospital where you spend most of your work time or provide most of your clinical services."
 
 # area/unit question
 fe = FormElement.create form_id: form.id, element_type_id: ElementType.find_by_e_type("radio").id, element_id: 2, element_name: "element_2_1", element_title: "What is your primary work area or unit in this hospital? Select ONE answer."
